00cd6bd084ae65fc8742a40c6b91e576fb57c4cb
federista <zaidrafi107@gmail.com>
2020-10-25 05:50:40 +0100

    removal

--- build/datavis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zaid/catkin_ws/src/datavis
-# Build directory: /home/zaid/catkin_ws/build/datavis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
